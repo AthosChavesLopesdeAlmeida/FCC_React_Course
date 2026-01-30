b93fa30644bd9de6fadc74df387cf4663cd49480
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-01-30 20:23:04 -0300

    30/01/2026

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Greeting () {
+  return (
+  <React.Fragment>
+    <div className = 'basic'>
+      <h1>Hello, world!</h1>
+      <p>This is my first experience with React!</p>
+    </div>
+  </React.Fragment>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Greeting/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
