395136c4acfdd8edf8336b8bcbc43cbf37f93dc3
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-01-31 21:25:37 -0300

    31/01/2026

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,37 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css'
+const books = [
+  {
+    author: 'Cícero',
+    title : 'Da República',
+    img : 'https://m.media-amazon.com/images/I/51mk+wNVllS._SL1200_.jpg',
+    id : 1
+  },
+  {
+    author: 'Cícero',
+    title : 'Dos Deveres',
+    img : 'https://m.media-amazon.com/images/I/51+OqVlfw2L._SL1050_.jpg',
+    id : 2
+  },
+  {
+    author: 'Cícero',
+    title : 'As Catilinárias',
+    img : 'https://m.media-amazon.com/images/I/61gwMjgqwkL._SL1056_.jpg',
+    id : 3
+  }
 
-const book1 = {
-  author: 'Cícero',
-  title : 'Da República',
-  img : 'https://m.media-amazon.com/images/I/51mk+wNVllS._SL1200_.jpg'
-}
-
-const book2 = {
-  author: 'Cícero',
-  title : 'Dos Deveres',
-  img : 'https://m.media-amazon.com/images/I/51+OqVlfw2L._SL1050_.jpg'
-}
-
-const book3 = {
-  author: 'Cícero',
-  title : 'As Catilinárias',
-  img : 'https://m.media-amazon.com/images/I/61gwMjgqwkL._SL1056_.jpg'
-}
+]
 
 function BookList () {
   return (
   <React.Fragment>
     <h1>Principais livros de Cícero</h1>
     <section className='evil_Larry'>
-      <Book author={book1.author} title={book1.title} img={book1.img}/>
-      <Book author={book2.author} title={book2.title} img={book2.img}/>
-      <Book author={book3.author} title={book3.title} img={book3.img}/>
+      {books.map((book) => {
+        const {img, title, author, id} = book
+        return <Book img={img} title={title} author={author} key={id}/>;
+      })}
     </section>
   </React.Fragment>
   );
 }
+
+
 const Book = ({img, title, author}) => {
   return (
     <article className='larry'>
