541db7d771db3f5c5fece7781adf8033a799e008
cahves1 <athoschaveslopesdealmeida@gmail.com>
2026-02-01 16:02:03 -0300

    01/02/2026

---
+++ tutorial/src/books.js
@@ -0,0 +1,25 @@
+import img1 from './imgs/img1.jpg'
+import img2 from './imgs/img2.jpg'
+import img3 from './imgs/img3.jpg'
+
+export const books = [
+  {
+    author: 'Cícero',
+    title : 'Da República',
+    img : img1,
+    id : 1
+  },
+  {
+    author: 'Cícero',
+    title : 'Dos Deveres',
+    img : img2,
+    id : 2
+  },
+  {
+    author: 'Cícero',
+    title : 'As Catilinárias',
+    img : img3,
+    id : 3
+  }
+  
+]
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,27 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css'
-const books = [
-  {
-    author: 'Cícero',
-    title : 'Da República',
-    img : 'https://m.media-amazon.com/images/I/51mk+wNVllS._SL1200_.jpg',
-    id : 1
-  },
-  {
-    author: 'Cícero',
-    title : 'Dos Deveres',
-    img : 'https://m.media-amazon.com/images/I/51+OqVlfw2L._SL1050_.jpg',
-    id : 2
-  },
-  {
-    author: 'Cícero',
-    title : 'As Catilinárias',
-    img : 'https://m.media-amazon.com/images/I/61gwMjgqwkL._SL1056_.jpg',
-    id : 3
-  }
-
-]
+import { books } from './books.js' // Books é um objeto, por isso os '{}'
+import Book from './book.js'
 
 function BookList () {
   return (
@@ -30,25 +11,12 @@ function BookList () {
     <h1>Principais livros de Cícero</h1>
     <section className='evil_Larry'>
       {books.map((book) => {
-        const {img, title, author, id} = book
-        return <Book img={img} title={title} author={author} key={id}/>;
+        return <Book {...book} key={book.id}/>;
       })}
     </section>
   </React.Fragment>
   );
 }
 
-
-const Book = ({img, title, author}) => {
-  return (
-    <article className='larry'>
-      <img src= {img} alt={title}/>
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BookList/>)
-
+root.render(<BookList/>) 
